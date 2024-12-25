c439285da0d0c4a87550ccab53a5990cdab88e7e
JunyiYe <34924397+JunyiYe@users.noreply.github.com>
2024-12-25 23:54:02 +0000

    Deploying to gh-pages from @ JunyiYe/JunyiYe.github.io@ab8cc8fd5e5e27ca2b3d89ddbcc3eae747ab93d8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,16 +431,16 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+    },{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-creativemath-is-accepted-by-aaai-2025",
+          title: 'CreativeMath is accepted by AAAI 2025.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
