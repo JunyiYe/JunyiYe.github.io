1a73d2d826c01487c467f32e22f5fdbcfe75e0e1
JunyiYe <34924397+JunyiYe@users.noreply.github.com>
2025-01-23 21:14:56 +0000

    Deploying to gh-pages from @ JunyiYe/junyiye.github.io@f5c1ce3ee5c4ba63ecf3b6953823d138eac3ced1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -397,32 +397,7 @@ ninja.data = [{
           section: "News",},{id: "news-our-paper-textflow-has-been-accepted-for-main-conference-at-naacl-2025",
           title: '🎉 Our paper, TextFlow, has been accepted for main conference at NAACL 2025!...',
           description: "",
-          section: "News",},{id: "projects-textflow",
-          title: 'TextFlow',
-          description: "a framework that converts flowchart images into text to improve explainability and control in flowchart understanding tasks",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/0_project/";
-            },},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
+          section: "News",},{id: "projects-project-5",
           title: 'project 5',
           description: "a project with a background image",
           section: "Projects",handler: () => {
